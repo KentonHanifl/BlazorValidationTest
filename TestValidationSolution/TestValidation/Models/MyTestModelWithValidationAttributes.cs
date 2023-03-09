d322e5757427ff20c5a69ea0ac91113822e0a717
KentonHanifl <kentonhanifl@gmail.com>
2023-03-09 11:51:12 -0600

    validation test

---
+++ TestValidationSolution/TestValidation/Models/MyTestModelWithValidationAttributes.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestValidation.Models
+{
+    public class MyTestModelWithValidationAttributes
+    {
+        [MaxLength(10)]
+        public string TestString { get; set; }
+    }
+}
